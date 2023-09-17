d6bff6bdbabbbc5b2c313dfd146c28e4b04b714f
Roonglit Chareonsupkul (Mac) <roonglit@gmail.com>
2023-09-17 14:30:30 +0700

    remove noindex which blocks robots to crawls the site

--- common/elements/head/HeadTitle.jsx
+++ common/elements/head/HeadTitle.jsx
@@ -6,7 +6,6 @@ const HeadTitle = ({pageTitle}) => {
       <meta charSet="utf-8" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <title>{`${pageTitle} || Tech Monday`}</title>
-      <meta name="robots" content="noindex, follow" />
       <meta name="description" content="Tech Monday" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <link rel="icon" type="image/x-icon" href={`${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH ?? '' : ''}/favicon.ico`} />
