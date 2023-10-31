dee1f1f11b6e196721b8442149a381725a83294e
Roonglit Chareonsupkul <roonglit@gmail.com>
2023-10-31 21:59:56 +0700

    add category page

---
+++ pages/categories/[slug].js
@@ -0,0 +1,72 @@
+import { getAllCategories, getCategoryBySlug, getCategories } from '../../lib/api/categories';
+import { getTags } from '../../lib/api/tags';
+import { getPopularPosts } from '../../lib/api/posts';
+import { getAuthors } from '../../lib/api/authors';
+
+import HeadTitle from "../../common/elements/head/HeadTitle";
+import HeaderOne from '../../common/elements/header/HeaderOne';
+import BreadcrumbOne from '../../common/elements/breadcrumb/breadcrumbOne';
+import PostLayoutTwo from '../../common/components/post/layout/PostLayoutTwo';
+import SidebarOne from '../../common/components/sidebar/SidebarOne';
+import FooterOne from '../../common/elements/footer/FooterOne';
+
+const Category = ({ category, categories, popularPosts, tags, authors }) => {
+	
+	return (
+		<>
+      <HeadTitle pageTitle={`${category.name}`} />
+      <HeaderOne postData={[]} />
+      <BreadcrumbOne title={category.name} />
+      <div className="axil-post-list-area axil-section-gap bg-color-white">
+        <div className="container">
+          <div className="row">
+          <div className="col-lg-8 col-xl-8">
+            <PostLayoutTwo dataPost={category.posts} show="5"/>
+          </div>
+          <div className="col-lg-4 col-xl-4 mt_md--40 mt_sm--40">
+            <SidebarOne dataPost={popularPosts} categories={categories}/>
+          </div>
+          </div>
+        </div>
+      </div>
+      <FooterOne categories={categories} tags={tags} authors={authors}/>
+		</>
+	);
+}
+
+export default Category;
+
+
+export async function getStaticProps({ params, preview }) {
+  const category = await getCategoryBySlug(params.slug, preview)
+  const categories = await getCategories(preview)
+  const popularPosts = await getPopularPosts(preview)
+  const tags = await getTags(preview)
+  const authors = await getAuthors(preview)
+
+  return {
+    props: {
+      category,
+      categories,
+      popularPosts,
+      tags,
+      authors
+    }
+  }
+}
+
+
+export async function getStaticPaths() {
+  const categories = await getAllCategories()
+
+  return {
+    paths: categories.map((category) => {
+      return {
+        params: {
+          slug: `${category.slug}`
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
