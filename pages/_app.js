a3a6b99b09b56b8dcc2d8c2b81af66cc9719eae2
Roonglit Chareonsupkul <roonglit@gmail.com>
2023-11-01 22:25:11 +0700

    add google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
+import Script from 'next/script'
+
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/style.scss';
 
+var GTM_KEY = process.env.NEXT_PUBLIC_GTM_KEY;
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Script id="google-tag-manager" strategy="afterInteractive">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_KEY}');
+      `}
+    </Script>
+    <Component {...pageProps} />;
+  </>
 }
--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,12 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <noscript 
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PV72JHLF"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          />
         </body>
       </Html>
     )
