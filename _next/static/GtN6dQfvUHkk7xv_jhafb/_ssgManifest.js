7f547adce1f238437b0ae870b6f71ae615b89d1d
roonglit <roonglit@gmail.com>
2024-01-22 14:57:38 +0000

    Deploying to gh-pages from @ techmonday-co/techmonday@39be9218fb5da884705a1609fa531a5762b977db 🚀

--- _next/static/GtN6dQfvUHkk7xv_jhafb/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fauthors\u002F[slug]","\u002Fcategories\u002F[slug]","\u002Fdevelopment","\u002Fposts\u002F[slug]","\u002Ftags\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
