b897d5b80ad0d1cbcc1d0a50492e9e925790ce84
Roonglit Chareonsupkul (Mac) <roonglit@gmail.com>
2024-02-19 14:09:40 +0700

    fix displaying link on the content

--- common/components/post/format/content/PostParagraph.jsx
+++ common/components/post/format/content/PostParagraph.jsx
@@ -1,16 +1,31 @@
 import PostLink from "./PostLink"
 
 const PostParagraph = ({ block }) => {
+  const renderContent = (content, index) => {
+    if (content.type === 'text') {
+      return <span key={`content-${index}`}>{content.text}</span>
+    } else if (content.type === 'image') {
+      return <img key={`content-${index}`} src={content.url} alt={content.alt} />
+    } else if (content.type === 'link-node') {
+      return <PostLink key={`content-${index}`} block={content} />
+    } else if (content.type === 'link') {
+      return (
+        <a key={`content-${index}`} href={content.url} target="_blank" rel="noopener noreferrer">
+          {
+            content.children.map((child, linkIndex) => {
+              return renderContent(child, index + '-link-' + linkIndex)
+            })
+          }
+        </a>
+        
+      )
+    }
+  }
+
   return (
     <p className="post-content">
       {block.children.map((child, index) => {
-        if (child.type === 'text') {
-          return <span key={`content-${index}`}>{child.text}</span>
-        } else if (child.type === 'image') {
-          return <img key={`content-${index}`} src={child.url} alt={child.alt} />
-        } else if (child.type === 'link-node') {
-          return <PostLink key={`content-${index}`} block={child} />
-        }
+        return renderContent(child, index)
       })}
     </p>
   )
