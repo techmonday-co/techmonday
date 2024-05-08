440fe24e15d673da894ad01ff5f437b19c570db0
Roonglit Chareonsupkul <roonglit@gmail.com>
2024-05-08 10:23:37 +0700

    Indexing - add script for submitting sitemaps (#2)
    * add indexing scripts
    
    * fix adding site for using sitemap api
    
    * update comment

--- scripts/indexing/publish.js
+++ scripts/indexing/publish.js
@@ -7,7 +7,10 @@ const jwtClient = new google.auth.JWT(
   key.client_email,
   null,
   key.private_key,
-  ["https://www.googleapis.com/auth/indexing"],
+  [
+    "https://www.googleapis.com/auth/indexing",
+    "https://www.googleapis.com/auth/webmasters" 
+  ],
   null
 );
 
@@ -19,21 +22,55 @@ jwtClient.authorize(function(err, tokens) {
   }
 
   // If the authorization is successful, set up the options for the request to the Google Search Index API
+  // let options = {
+  //   url: "https://indexing.googleapis.com/v3/urlNotifications:publish",
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   auth: { "bearer": tokens.access_token },
+  //   json: {
+  //     "url": "http://techmonday.co",
+  //     "type": "URL_UPDATED"
+  //   }
+  // };
+
+  // // Make the request and handle the response
+  // request(options, function (error, response, body) {
+  //   console.log(body);
+  // });
+
+  // Submit sitemaps
+  var siteUrl = encodeURIComponent("https://techmonday.co");
+  var feedpath = encodeURIComponent("https://techmonday.co/sitemap.xml");
   let options = {
-    url: "https://indexing.googleapis.com/v3/urlNotifications:publish",
-    method: "POST",
+    url: `https://www.googleapis.com/webmasters/v3/sites/${siteUrl}/sitemaps/${feedpath}`,
+    method: "PUT",
     headers: {
       "Content-Type": "application/json"
     },
-    auth: { "bearer": tokens.access_token },
-    json: {
-      "url": "http://techmonday.co",
-      "type": "URL_UPDATED"
-    }
+    auth: { "bearer": tokens.access_token }
   };
 
   // Step 8: Make the request and handle the response
   request(options, function (error, response, body) {
     console.log(body);
   });
+
+  // Add site to Search Console, we need to add this before we can submit sitemaps
+  // var siteUrl = encodeURIComponent("https://techmonday.co");
+  // var feedpath = encodeURIComponent("https://techmonday.co/sitemap.xml");
+  // let options = {
+  //   url: `https://www.googleapis.com/webmasters/v3/sites/${siteUrl}`,
+  //   method: "PUT",
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   auth: { "bearer": tokens.access_token }
+  // };
+
+  // // Step 8: Make the request and handle the response
+  // request(options, function (error, response, body) {
+  //   console.log(body);
+  // });
 });
